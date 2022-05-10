47512367d47e0491d8387797793c7f9a95d96529
loren <lorenmichaelh@gmail.com>
2022-05-10 15:23:31 -0500

    troubleshooting BrowserRouter wrap did not work

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
@@ -7,11 +7,11 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  // <Router>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </BrowserRouter>
+  // </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
