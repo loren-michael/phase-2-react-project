19fa94ec274d2ed552b5cd24ed281e95f3b82c81
loren <lorenmichaelh@gmail.com>
2022-05-22 21:44:58 -0500

    recipe form changed to divs

--- src/components/RecipeForm.js
+++ src/components/RecipeForm.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Form } from 'semantic-ui-react';
 
 function RecipeForm({ onRecipeSubmit }) {
     const [newRecipe, setNewRecipe] = useState({
@@ -22,81 +21,126 @@ function RecipeForm({ onRecipeSubmit }) {
         console.log(newRecipe)
     }
 
-    function handleRecipeSubmit(e) {
-        e.preventDefailt();
+    function handleAddIng(e) {
+        const lineBreak = document.createElement("br");
+        const newIngBox = document.createElement("input");
+        newIngBox.className = "add-ingredient";
+        newIngBox.name = "add-ingredient";
+        const addButton = e.target.parentNode;
+        addButton.insertBefore(newIngBox, e.target);
+        addButton.insertBefore(lineBreak, e.target);
+        
+        
+        // const newIng = <div>Test</div>;
+        // console.log(e.target.parentNode)
+        // e.target.parentNode.insertBefore(newIng, e.target)
+    }
 
-        console.log("Hello from recipe submit")
+    function handleAddIns(e) {
+        console.log(e.target.parentNode)
+    }
+
+    function handleRecipeSubmit(e) {
+        e.preventDefault();
+        console.log(newRecipe)
     }
 
 
     return (
         <div id="recipe-form" >
-            <Form>
-                <Form.Field id="add-name" >
-                    <label>Recipe Title:  </label>
-                    <input 
-                        name="name"
-                        required
-                        placeholder="recipe name" 
-                        value={newRecipe.name} 
-                        onChange={handleChange}
-                    />
-                </Form.Field>
-                <Form.Field id="add-image" >
-                    <label>Image URL:  </label>
-                    <input required placeholder="image URL" name="img" onChange={handleChange} />
-                </Form.Field>
-                <Form.Field id="add-author" >
-                    <label>Author:  </label>
-                    <input placeholder="author's name" name="author" onChange={handleChange} />
-                </Form.Field>
-                <Form.Field id="add-source" >
-                    <label>Source Website:  </label>
-                    <input placeholder="link to original recipe" name="source" onChange={handleChange} />
-                </Form.Field>
-                <Form.Field id="add-mealtype" >
-                    <label>Meal Type:  </label>
-                    <select required name="mealtype" onChange={handleChange} >
-                        <option></option>
-                        <option>Dinner</option>
-                        <option>Lunch</option>
-                        <option>Breakfast</option>
-                        <option>Snack</option>
-                    </select>
-                </Form.Field>
-                <Form.Field id="add-preptime" >
-                    <label>Prep time:  </label>
-                    <input placeholder="prep time" name="preptime" onChange={handleChange} />
-                </Form.Field>
-                <Form.Field id="add-cooktime" >
-                    <label>Cook time:  </label>
-                    <input placeholder="cook time" name="cooktime" onChange={handleChange} />
-                </Form.Field>
-                <Form.Field id="add-servings" >
-                    <label>Servings:  </label>
-                    <input placeholder="number of servings" name="servings" onChange={handleChange} />
-                </Form.Field>
-                <Form.Field id="add-ingredient" >
-                    <label>Ingredients:  </label>
-                    <input type="text" />
-                    <br></br>
-                    <button type="button" id="btn-add-ingredient" > Add Ingredient </button>
-                </Form.Field>
-                <Form.Field id="add-instructions" >
-                    <label>Instructions:  </label>
-                    <input type="text" />
-                    <br></br>
-                    <button type="button" id="btn-add-instructions" > Add Instruction </button>
-                </Form.Field>
-                <Form.Field id="init-comm" >
-                    <label>Comment:  </label>
-                    <input placeholder="leave a note" />
-                </Form.Field>
+            <div id="add-name" >
+                <label>Recipe Title:  </label>
+                <input 
+                    name="name"
+                    placeholder="recipe name" 
+                    value={newRecipe.name} 
+                    onChange={handleChange}
+                />
+            </div>
+            <div id="add-image" >
+                <label>Image URL:  </label>
+                <input 
+                    placeholder="image URL"
+                    name="img"
+                    value={newRecipe.img}
+                    onChange={handleChange}
+                />
+            </div>
+            <div id="add-author" >
+                <label>Author:  </label>
+                <input
+                    placeholder="author's name"
+                    name="author" 
+                    value={newRecipe.author}
+                    onChange={handleChange}
+                />
+            </div>
+            <div id="add-source" >
+                <label>Source Website:  </label>
+                <input 
+                    placeholder="link to original recipe"
+                    name="source" 
+                    value={newRecipe.source}
+                    onChange={handleChange}
+                />
+            </div>
+            <div id="add-mealtype" >
+                <label>Meal Type:  </label>
+                <select name="mealtype" value={newRecipe.mealtype} onChange={handleChange} >
+                    <option></option>
+                    <option>Dinner</option>
+                    <option>Lunch</option>
+                    <option>Breakfast</option>
+                    <option>Snack</option>
+                </select>
+            </div>
+            <div id="add-preptime" >
+                <label>Prep time:  </label>
+                <input
+                    placeholder="prep time"
+                    name="preptime"
+                    value={newRecipe.preptime}
+                    onChange={handleChange} 
+                />
+            </div>
+            <div id="add-cooktime" >
+                <label>Cook time:  </label>
+                <input 
+                    placeholder="cook time"
+                    name="cooktime"
+                    value={newRecipe.cooktime}
+                    onChange={handleChange} 
+                />
+            </div>
+            <div id="add-servings" >
+                <label>Servings:  </label>
+                <input 
+                    placeholder="number of servings"
+                    name="servings"
+                    value={newRecipe.servings}
+                    onChange={handleChange} 
+                />
+            </div>
+            <div id="add-ingredient" >
+                <label>Ingredients:  </label>
+                <input type="text" />
                 <br></br>
-                <Form.Field>
-                    <button type="submit" onClick={handleRecipeSubmit} > Submit this recipe! </button>
-                </Form.Field>
-            </Form>
+                <button type="button" id="btn-add-ingredient" onClick={handleAddIng} > Add Ingredient </button>
+            </div>
+            <div id="add-instructions" >
+                <label>Instructions:  </label>
+                <input type="text" />
+                <br></br>
+                <button type="button" id="btn-add-instructions" onClick={handleAddIns} > Add Instruction </button>
+            </div>
+            <div id="init-comm" >
+                <label>Comment:  </label>
+                <input placeholder="leave a note" />
+            </div>
+            <br></br>
+            <div>
+                <button type="submit" onSubmit={handleRecipeSubmit} > Submit this recipe! </button>
+            </div>
         </div>
     )
 }
